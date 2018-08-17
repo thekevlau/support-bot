0ab25ab02f095b548e7ebc30d4ef75b26508be44
Kevin Lau <thekevlau@gmail.com>
2018-08-16 17:36:18 -0700

    first commit

---
+++ handle_problems.js
@@ -0,0 +1,34 @@
+module.exports = { 
+
+	handle_safety: function(){
+	// if safety concern
+		console.log("If you feel your safety is compromised please call 911 immediately");
+		console.log("If you would like to speak to a specialist about ");
+
+	},
+	handle_lost_item: function() {
+		console.log("Please contact your driver at DRIVER_PROXY_NUMBER to coordinate pickup.");
+	},
+	clarify_safety: function() {
+		console.log("wait I need more info. If you feel unsafe please say you feel unsafe.");
+	}
+
+// function handle_rude(){
+// 	// 
+// 	console.log("you being rude homie? that's no gucci");
+// }
+
+// function handle_lost_item(){
+// 	// I lost something
+// 	// please contact your driver at SOME_PROXY_NUMBER
+// 	// contact driver, return lost item, lost item return fee
+// 	let lost_response = "Please contact your driver at DRIVER_PROXY_NUMBER to coordinate pickup.";
+// 	console.log(lost_response);
+// }
+
+};
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const {Wit, log} = require('node-wit');
+const app = express();
+
+var handle_problems = require('./handle_problems.js');
+
+const client = new Wit({accessToken: 'FJC4JZDUKOJTYXL4Z7XMVBJOFM2NPPQN'});
+
+
+// app.get('/', function (req, res) {
+// 	res.send('Hello World!');
+// });
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
+});
+
+app.post('/', function (req, res) {
+	console.log("why hello there");
+	console.log(req);
+});
+
+function ask_wit_ai(query) {
+	client.message(query, {})
+		.then((data) => {
+			let string_result = JSON.stringify(data);
+			console.log('Yay, got Wit.ai response: ' + string_result);
+			// safety first
+			if (data.entities.hasOwnProperty('safety')){
+				handle_problems.handle_safety();
+			} else if (data.entities.hasOwnProperty('lost_things')) {
+				handle_problems.handle_lost_item();
+			} else if (data.entities.hasOwnProperty('sentiment')) {
+				if (data.entities.sentiment.value = "negative") {
+					handle_problems.clarify_safety();
+				}
+			} else {
+
+			}
+		})
+		.catch(console.error);
+}
+
+// INPUT FROM TEXT
+
+
+
+
+
+// INPUT FROM STDIN
+
+// Get process.stdin as the standard input object.
+var standard_input = process.stdin;
+
+// Set input character encoding.
+standard_input.setEncoding('utf-8');
+
+// Prompt user to input data in console.
+console.log("Hello I am UberBot. Please tell me your problems by inputting text:");
+
+// When user input data and click enter key.
+standard_input.on('data', function (data) {
+
+	// User input exit.
+	if(data === 'exit\n'){
+		// Program exit.
+		console.log("User input complete, program exit.");
+		process.exit();
+	} else {
+		// Process Input
+		ask_wit_ai(data);
+
+	}
+});
+
+
+// use case
+// make a thing that helps define responses to consumer complaints
+
+
+
+
+
